2efbbd70a4e6b0a0604826f94e258748c3259e31
Arroyo <johnniaa@amazon.com>
2022-06-29 15:06:48 -0700

    add signup component and userpool

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import Signup from './authComponents/signUp';
 function App() {
   return (
     <div className="App">
@@ -16,6 +16,9 @@ function App() {
         >
         </a>
       </header>
+      <div>
+        <Signup />
+      </div>
     </div>
   );
 }
