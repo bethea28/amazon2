ea624410e57e129de54b1e2ff10303a2b00a11bb
Vairavan <sornamv@88665a374e52.ant.amazon.com>
2022-07-06 10:53:51 -0700

    integrated react hook forms

--- src/components/Profile/ProfileForm.tsx
+++ src/components/Profile/ProfileForm.tsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import { useForm } from 'react-hook-form'
+
 import {
     Container,
     Paper,
@@ -10,10 +12,21 @@ import {
     Button
 } from "@mui/material"
 
+
+type FormData = {
+    name: string,
+    bio: string,
+}
+
 export default function ProfileForm() {
 
-    const [name, setName] = useState("")
-    const [bio, setBio] = useState("")
+    const {register,
+        handleSubmit,
+        formState: {errors} 
+    } = useForm<FormData>()
+
+    const onSubmit = handleSubmit(data => console.log(data))
+
     const [interests, setInterests] = useState([{"name": "Art", "hasInterest": true}, 
                                                 {"name": "Comics and Illustrations", "hasInterest": false}, 
                                                 {"name": "Film", "hasInterest": false}, 
@@ -23,14 +36,28 @@ export default function ProfileForm() {
                                                 {"name": "Music", "hasInterest": false}, 
                                                 {"name": "Publishing", "hasInterest": true}])
 
-    const handleChange = (event) => {
+    console.log(errors)
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         
+        // find index of matched object using the name
+        const idx = interests.findIndex((interest => interest.name === event.target.name));
+        
+        // unselect -> mark it false
         if (event.target.checked !== true) {
-            setInterests({...interests, [event.target.name]: event.target.checked})
-        } else {
-            setInterests({...interests, [event.target.name]: event.target.checked})
+            
+            interests[idx]['hasInterest'] = false;
+            console.log(interests[idx])
         }
+        // select -> mark it true
+        else {
+            interests[idx]['hasInterest'] = true;
+            console.log(interests[idx])
+        }
+        event.target.defaultChecked = !event.target.defaultChecked;
+        setInterests(interests);
     }
+
 
     return (
         <Container maxWidth="xs">
@@ -38,33 +65,33 @@ export default function ProfileForm() {
             <Grid container direction="column">
                 <Grid item>
                     <TextField
+                        {...register("name", {required: "Name is required", minLength: {value: 3, message: 'Name should be at least 3 letters'}})}
                         variant="outlined"
                         label="Name"
                         name="name"
                         size="small"
                         margin="dense"
-                        value={name}
-                        required
                     />
                 </Grid>
                 <Grid item>
                     <TextField
+                        {...register("bio")}
                         variant="outlined"
                         label="Bio"
                         name="bio"
-                        value={bio}
                         margin="dense"
                     />
                 </Grid>
                 <Grid item>
                     <FormGroup>
+                    {/* {...register("interests")} */}
                     {interests.map((interest) => {
-                    return <FormControlLabel control={<Checkbox defaultChecked={interest.hasInterest} name={interest.name} />} label={interest.name} key={interest.name}/>
+                    return <FormControlLabel control={<Checkbox defaultChecked={interest.hasInterest} name={interest.name} onChange={handleChange} />} label={interest.name} key={interest.name}/>
                     })}
                     </FormGroup>
                 </Grid>
                 <Grid item>
-                    <Button variant="contained">Save Changes</Button>
+                    <Button variant="contained" onClick={onSubmit} >Save Changes</Button>
                 </Grid>
                 
             </Grid>
