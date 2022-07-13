1cb67f754bad86b505489ae9e9cfa672bd1af482
Nabua <lnabua@amazon.com>
2022-07-13 09:03:16 -0700

    created project card and project details

---
+++ src/Components/Project/ProjectCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+    FavoriteBorderOutlined,
+    MonetizationOnOutlined
+} from '@mui/icons-material';
+import {
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    Button,
+    Typography,
+    IconButton,
+    Container,
+    Paper
+} from "@mui/material";
+
+const ProjectCard = () => {
+    //later to include (props: any) in params
+
+    return (
+        <Container maxWidth="xs">
+            <Paper>
+                <Card variant='outlined'>
+                    <CardActionArea>
+                        <img alt='project img' src='https://picsum.photos/400/300' />
+                        <CardContent>
+                            <Typography gutterBottom variant="h4">
+                                Project Name
+                                {/* {props.name} */}
+                            </Typography>
+                            <Typography variant='h6' gutterBottom>
+                                Project Description
+                                {/* {props.description} */}
+                            </Typography>
+                        </CardContent>
+                        <CardContent>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                By User Name
+                                {/* {props.userId.name} */}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button variant='outlined' size="small" color="primary">
+                            Back
+                        </Button>
+                        <IconButton>
+                            <MonetizationOnOutlined />
+                        </IconButton>
+                        <IconButton>
+                            <FavoriteBorderOutlined />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            </Paper>
+        </Container>
+    );
+}
+export default ProjectCard;
---
+++ src/Components/Project/ProjectDetails.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { FavoriteBorderOutlined } from '@mui/icons-material';
+import {
+    Container,
+    Grid,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    Button,
+    Typography,
+    IconButton,
+    Paper
+} from "@mui/material";
+
+const ProjectDetails = () => {
+    //later to include (props: any) in params
+
+    return (
+        <Container maxWidth="lg">
+            <Grid container spacing={6} justifyItems={"center"}>
+                <Grid item>
+                    <Paper style={{ padding: 20 }}>
+                        <img alt='project img' src='https://picsum.photos/400/300' />
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper style={{ padding: 20 }}>
+                        <Card variant='outlined'>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h4">
+                                        Project Name
+                                        {/* {props.name} */}
+                                    </Typography>
+                                    <Typography variant='h6' gutterBottom>
+                                        Project Description
+                                        {/* {props.description} */}
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        By User Name
+                                        {/* {props.userId.name} */}
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Target Funding Amount
+                                        {/* {props.targetFundingAmount} */}
+                                    </Typography>
+                                </CardContent>
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        Target Funding Date
+                                        {/* {props.targetFundingDate} */}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button variant='outlined' size="small" color="primary">
+                                    Back this project
+                                </Button>
+                                <IconButton>
+                                    <FavoriteBorderOutlined />
+                                </IconButton>
+                            </CardActions>
+                        </Card>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </Container>
+
+    )
+}
+export default ProjectDetails;
