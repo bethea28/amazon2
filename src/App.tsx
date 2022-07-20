8afda43482ca67844208cf0cf7c4744b16d036cc
151177 <36523610+151177@users.noreply.github.com>
2022-07-20 11:25:54 -0700

    re-added fe elements

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { v4 as uuid } from "uuid";
 import { Box, Typography, Link, Grid } from "@mui/material";
 import CreateProjectForm from "./Components/Project/CreateProjectForm";
 
-export function LogInOrSignUp() {
+function App() {
   const [user, setUser] = useState<string | null>(null);
   const [msgAlerts, setMsgAlerts] = useState<
     (AutoDismissAlertProps & { id: string })[]
@@ -30,58 +30,47 @@ export function LogInOrSignUp() {
   };
   return (
     <>
-      <Grid
-        container
-        spacing={0}
-        justifyContent='center'
-        alignItems='center'
-        paddingTop={10}
-      >
-        <Box>
-          <header className='App-header'>
-            {msgAlerts.map((msgAlert) => (
-              <AutoDismissAlert key={msgAlert.id} {...msgAlert} />
-            ))}
-            <BrowserRouter>
-              <Routes>
-                <Route
-                  path='/signup'
-                  element={<Signup setUser={setUser} msgAlert={msgAlert} />}
-                />
-                <Route
-                  path='/signin'
-                  element={<SignIn setUser={setUser} msgAlert={msgAlert} />}
-                />
-                <Route path='/profile/:userId' element={<Profile />} />
-                <Route path='/profile/:userId/edit' element={<ProfileForm />} />
-                <Route path='/createProject' element={<CreateProjectForm />} />
-              </Routes>
-            </BrowserRouter>
-            <Link
-              className='App-link'
-              href='https://reactjs.org'
-              target='_blank'
-              rel='noopener noreferrer'
-            ></Link>
-          </header>
-        </Box>
-      </Grid>
-    </>
-  );
-}
-
-function App() {
-  return (
-    <>
+      <Box>
+        <header className='App-header'>
+          {msgAlerts.map((msgAlert) => (
+            <AutoDismissAlert key={msgAlert.id} {...msgAlert} />
+          ))}
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path='/signup'
+                element={<Signup setUser={setUser} msgAlert={msgAlert} />}
+              />
+              <Route
+                path='/signin'
+                element={<SignIn setUser={setUser} msgAlert={msgAlert} />}
+              />
+              <Route path='/profile/:userId' element={<Profile />} />
+              <Route path='/profile/:userId/edit' element={<ProfileForm />} />
+              <Route path='/createProject' element={<CreateProjectForm />} />
+            </Routes>
+          </BrowserRouter>
+          {/* <SignIn msgAlert={msgAlert} setUser={setUser} />
+          <Signup msgAlert={msgAlert} setUser={setUser} /> */}
+          <Link
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          ></Link>
+        </header>
+      </Box>
       <NavigationBar />
-      <LogInOrSignUp />
       <Grid container spacing={0} justifyContent='center' alignItems='center'>
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/projectId' element={<Comment />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/projectId' element={<CommentForm />} />
+          </Routes>
+        </BrowserRouter>
       </Grid>
       <Footer />
+      <Comment />
     </>
   );
 }
