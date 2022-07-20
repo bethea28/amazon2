24cf85e4d513d46ef7a2ea9816dd67afd41a6367
Vairavan <sornamv@88665a374e52.ant.amazon.com>
2022-07-20 09:56:07 -0700

    added Discover file

--- src/Components/Discover/index.tsx
+++ src/Components/Discover/index.tsx
@@ -1,15 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
+import React from 'react';
 import {
-    Container,
-    Grid,
     Typography,
-    Avatar,
-    Paper,
-    Box,
-    IconButton,
-    List,
-    ListItem
 } from "@mui/material"
 
 export default function Discover() {
