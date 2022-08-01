f0c8f356a487e04dc0615c0031b07c2b72ca8921
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-07-31 19:51:10 -0700

    add optional coverImage Url for project

--- src/types/Project.ts
+++ src/types/Project.ts
@@ -1,12 +1,13 @@
 export default interface ProjectData {
-  projectId?: string,
-  userId?: string,
-  createdAt?: Date,
-  projectName?: string,
-  description?: string,
-  targetFundingAmount: number,
-  targetFundingDate: Date,
-  category: string,
-  images: Array<string>,
-  likedBy: Array<string>
+  projectId?: string;
+  userId?: string;
+  createdAt?: Date;
+  projectName?: string;
+  description?: string;
+  targetFundingAmount: number;
+  targetFundingDate: Date;
+  category: string;
+  coverImage?: string;
+  images: Array<string>;
+  likedBy: Array<string>;
 }
