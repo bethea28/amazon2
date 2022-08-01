92ed43097b508ad3bdb65396792751b9244a5b2d
Carolina Lunardi Correa <88406130+Arete-Carolina@users.noreply.github.com>
2022-07-31 19:56:24 -0700

    Updated upload service to return image URL.

--- src/services/UploadService.ts
+++ src/services/UploadService.ts
@@ -7,20 +7,16 @@ type FileUploadData = {
   file: File;
 };
 
-const {
-  REACT_APP_AWS_SECRET_KEY,
-  REACT_APP_AWS_REGION,
-  REACT_APP_AWS_ACCESS_KEY,
-  REACT_APP_AWS_BUCKET_NAME,
-} = process.env;
+const { REACT_APP_AWS_SECRET_KEY, REACT_APP_AWS_REGION, REACT_APP_AWS_ACCESS_KEY, REACT_APP_AWS_BUCKET_NAME } =
+  process.env;
 
-export const uploadToS3 = (data: FileUploadData): void => {
+export const uploadToS3 = async (data: FileUploadData) => {
   AWS.config.update({
     accessKeyId: REACT_APP_AWS_ACCESS_KEY,
     secretAccessKey: REACT_APP_AWS_SECRET_KEY,
   });
 
-  const myBucket = new AWS.S3({
+  const s3Object = new AWS.S3({
     params: { Bucket: REACT_APP_AWS_BUCKET_NAME },
     region: REACT_APP_AWS_REGION,
   });
@@ -32,18 +28,13 @@ export const uploadToS3 = (data: FileUploadData): void => {
     Key: data.file.name,
   };
 
-  myBucket
-    .putObject(params)
-    .on('success', () => {
-      const uploadedFileUrl = myBucket.getSignedUrl('getObject', { Key: params.Key });
-      const userData: UserData = {} as UserData;
-      const userId = '123'; // Add user ID of the logged in user | Or use a hardcoded one from your DB to test the API call
-      userData.profileImageUrl = uploadedFileUrl;
-      UserService.updateProfile(userData, userId)
-        .then(() => console.log('Saving to backend was successful'))
-        .catch(() => console.log('Saving to backend failed'));
+  const s3UploadResponse = await s3Object
+    .upload({
+      Bucket: REACT_APP_AWS_BUCKET_NAME || 'None',
+      Key: data.file.name,
+      Body: data.file,
     })
-    .send((err) => {
-      if (err) console.log(err);
-    });
+    .promise();
+
+  return s3UploadResponse.Location;
 };
