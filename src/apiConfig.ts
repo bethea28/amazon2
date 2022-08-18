a1a2d2606cf63e9692e2ecae3740f0b2b6815886
Vairavan <sornamv@88665a374e52.ant.amazon.com>
2022-08-17 17:43:11 -0700

    added accessToken to authorization header

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -7,7 +7,7 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
     "Content-Type": "application/json",
-    // "Authorization": `Bearer ${token}`
+    "Authorization": `Bearer ${token}`
 }
 });
 
