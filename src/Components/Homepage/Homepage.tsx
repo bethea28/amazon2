49aa89c802bcce7b8c2d5f54266d80549b31631b
Tra <trab.die@gmail.com>
2022-08-26 14:17:18 -0400

    update fix errors

--- src/Components/Homepage/Homepage.tsx
+++ src/Components/Homepage/Homepage.tsx
@@ -1,84 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Button, Grid, Typography } from "@mui/material";
 import ProjectCard from "../Project/ProjectCard";
-import { useNavigate } from 'react-router-dom';
-import ProjectData from '../../types/Project'
-import ProjectService from '../../services/ProjectService'
-import Carousel from 'react-material-ui-carousel';
-import NewProjects from './NewProjects';
-import TrendingProjects from './TrendingProjects';
+import { useNavigate } from "react-router-dom";
+import ProjectData from "../../types/Project";
+import ProjectService from "../../services/ProjectService";
+import Carousel from "react-material-ui-carousel";
+import NewProjects from "./NewProjects";
+import TrendingProjects from "./TrendingProjects";
 
 const Homepage = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const exploreProjects = () => {
-    navigate(`/projects`)
-  }
+    navigate(`/projects`);
+  };
 
-  const [allProjects, setAllProjects] = useState<Array<ProjectData> | []>()
+  const [allProjects, setAllProjects] = useState<Array<ProjectData> | []>();
 
   useEffect(() => {
-    fetchProjects()
-  }, [])
+    fetchProjects();
+  }, []);
 
   const fetchProjects = async () => {
-    await ProjectService.getAllProjects()
-    .then((response) => {setAllProjects(response.data)})
-  }
+    await ProjectService.getAllProjects().then((response) => {
+      setAllProjects(response.data);
+    });
+  };
 
   let topThreeProjects;
   if (allProjects) {
-    topThreeProjects = allProjects.slice(0,3);
+    topThreeProjects = allProjects.slice(0, 3);
   }
- 
+
   return (
     <>
       <Grid
         container
         spacing={0}
-        justifyContent='center'
-        alignItems='center'
-        flexDirection='column'
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
       >
         <Grid marginTop={2}>
-          <Typography sx={{ fontWeight: "bold" }} variant='h3'>
+          <Typography sx={{ fontWeight: "bold" }} variant="h3">
             Making Creative Ideas a Reality
           </Typography>
         </Grid>
         <Grid marginTop={4}>
-          <Button variant='contained' onClick={exploreProjects}>Explore Projects</Button>
+          <Button variant="contained" onClick={exploreProjects}>
+            Explore Projects
+          </Button>
         </Grid>
       </Grid>
 
-
-        <Grid marginTop={16} marginBottom={2}>
-          <Typography sx={{ fontWeight: "bold" }} variant='caption'>
-            THE LATEST
-          </Typography>
-        </Grid>
-
-        <Grid
-        >
-
-          <NewProjects/>
-        </Grid>
-      <Grid container justifyContent='center' alignItems='center' marginTop={2}>
-      {topThreeProjects &&
-        topThreeProjects.length > 0 &&
-        topThreeProjects.map((project) => {
-          return (
-            <Grid key={project.projectId} marginBottom={2}>
-              <ProjectCard {...project} />
-            </Grid>
-          )
-        })}
-
-        <Grid>
-          {/* <Trending component here/>*/}
-        </Grid>
+      <Grid>
+        <NewProjects />
       </Grid>
-  
+      <Grid>
+        <TrendingProjects />
+      </Grid>
     </>
   );
 };
